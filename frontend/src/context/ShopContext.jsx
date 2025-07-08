8aa5616d537f0d2c278506f65443f30a94f343c3
caotrinhthinh <caotrinhthinh@gmail.com>
2025-07-08 11:23:14 +0700

    Add Components Hero

---
+++ frontend/src/context/ShopContext.jsx
@@ -0,0 +1,21 @@
+import { createContext } from "react";
+import { products } from "../assets/frontend_assets/assets";
+
+const ShopContext = createContext();
+
+const ShopContextProvider = (props) => {
+    const currency = '$';
+    const delivery_fee = 10;
+
+    const value = {
+        products, currency, delivery_fee
+    }
+
+    return (
+        <ShopContext.Provider>
+            {props.children}
+        </ShopContext.Provider>
+    )
+}
+
+export default ShopContextProvider;
