0df0089fb05cea02df0022ea43bd078aac9c47e9
caotrinhthinh <caotrinhthinh@gmail.com>
2025-07-11 22:13:08 +0700

    Update page About

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -45,17 +45,26 @@ const About = () => {
       <div className="flex flex-col md:flex-row text-sm mb-20">
         <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
           <b>Quality Assurance:</b>
-          <p className="text-gray-6000"></p>
+          <p className="text-gray-6000">
+            We meticulously select and vet each product to ensure it meets our
+            stringent quality standards.
+          </p>
         </div>
 
         <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
           <b>Quality Assurance:</b>
-          <p className="text-gray-6000"></p>
+          <p className="text-gray-6000">
+            With our user-friendly interface and hassle-free ordering process,
+            shopping has never been easier.
+          </p>
         </div>
 
         <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
           <b>Quality Assurance:</b>
-          <p className="text-gray-6000"></p>
+          <p className="text-gray-6000">
+            Our team of dedicated professionals is here to assist you the way,
+            ensuring your satisfaction is our top priority.
+          </p>
         </div>
       </div>
 
